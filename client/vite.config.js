14f8df10c888324d0df15bc09c1171d61953a889
Sushi <shaheer.anser@gmail.com>
2024-07-19 23:09:39 +0500

    updated vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "http://library.riphah.khanewal.vercel.app",
         secure: false,
       },
     },
